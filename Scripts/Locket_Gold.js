b8485c3c87457507f5ffe2a3be8c517b55ece79a
hovantuan24354 <105086073+hovantuan24354@users.noreply.github.com>
2026-06-13 00:31:57 +0700

    Create Locket_Gold.js

---
+++ Scripts/Locket_Gold.js
@@ -0,0 +1,6 @@
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Hồ Văn Tuấn Locket Gold Shadowrocket";var locket02={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2222-02-22T22:22:22Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-02-22T22:22:22Z",purchase_date:"2022-02-22T22:22:22Z",store:"app_store"},locket01={grace_period_expires_date:null,purchase_date:"2022-02-22T22:22:22Z",product_identifier:"com.locket02.premium.yearly",expires_date:"2222-02-22T22:22:22Z"};const match=Object.keys(mapping).find
